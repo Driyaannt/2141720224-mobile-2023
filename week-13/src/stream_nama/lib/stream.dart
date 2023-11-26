1d61c800808e0940406786a9ba4a575a1c25ba7c
Driyaannt <49848545+Driyaannt@users.noreply.github.com>
2023-11-26 21:06:52 +0700

    W13: Jawaban Soal 6

--- week-13/src/stream_nama/lib/stream.dart
+++ week-13/src/stream_nama/lib/stream.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class ColorStream{
+class ColorStream {
   final List<Color> colors = [
     Colors.blueGrey,
     Colors.amber,
     Colors.deepPurple,
     Colors.lightBlue,
     Colors.teal,
-    Colors.pink,
-    Colors.red,
-    Colors.green,
-    Colors.orange,
-    Colors.purple,
+    Colors.lightGreenAccent,
+    Colors.deepOrange,
+    Colors.deepOrangeAccent,
+    Colors.lime,
+    Colors.lightGreen,
   ];
-
 
   Stream<Color> getColors() async* {
     yield* Stream.periodic(const Duration(seconds: 1), (int t) {
@@ -21,7 +21,18 @@ class ColorStream{
       return colors[index];
     });
   }
+}
+
+class NumberStream {
+  final StreamController<int> controller = StreamController<int>();
+
+  void addNumberToSink(int newNumber) {
+    controller.sink.add(newNumber);
+  }
+
+  close() {
+    controller.close();
+  }
 
   
-
 }
